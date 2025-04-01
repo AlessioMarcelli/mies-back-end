1784cf4e48d71d0cf5cb2f06d5f69864574fac98
Alessio <alessiomarcelli04@gmail.com>
2025-04-01 17:03:51 +0200

    fix

--- src/main/java/miesgroup/mies/webdev/Rest/Model/CostoArticoloResponse.java
+++ src/main/java/miesgroup/mies/webdev/Rest/Model/CostoArticoloResponse.java
@@ -7,11 +7,13 @@ public class CostoArticoloResponse {
     public Double costoUnitario;
     public String mese;
     public String nomeBolletta;
+    public String categoriaArticolo;
 
     public CostoArticoloResponse(CostoArticolo c) {
         this.nomeArticolo = c.getNomeArticolo();
         this.costoUnitario = c.getCostoUnitario();
         this.mese = c.getMese();
         this.nomeBolletta = c.getBolletta().getNomeBolletta(); // o getNome() se hai modificato il nome del metodo
+        this.categoriaArticolo = c.getCategoriaArticolo();
     }
 }
